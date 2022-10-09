bac29e420effc7ac0b2555e2e3849b566631964b
alireza asrari <alirezaasrari1990@gmail.com>
2022-10-09 09:45:18 +0330

    add date propertiy to the second page model

--- Back-End/TaskTime/TaskTime/Models/LastPage.cs
+++ Back-End/TaskTime/TaskTime/Models/LastPage.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public int Stars { get; set; }
         public string Description { get; set; }
+        public DateTime date { get; set; } = DateTime.Now;
 
         //navigation properties
         public int EmployeeId { get; set; }
